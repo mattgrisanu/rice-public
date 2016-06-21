848c16764a38234e1c20cc99901de568561557d9
Daniel Fiore <taptapdan@gmail.com>
2016-06-21 14:57:47 -0700

    Remove Bootstrap theme and use vanilla Bootstrap

--- static/client/src/modules/Landing-View/components/NavBar.js
+++ static/client/src/modules/Landing-View/components/NavBar.js
@@ -6,14 +6,16 @@ export default class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-default">
-        <div className="navbar-header">
-          <Link className="navbar-brand" to="/">Rice</Link>
-        </div>
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Rice</Link>
+          </div>
 
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
-            <li><Link to="/onboarding/preferences" className="pure-menu-link">Start Onboarding preferences</Link></li>
-          </ul>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li><Link to="/onboarding/preferences" className="pure-menu-link">Start Onboarding preferences</Link></li>
+            </ul>
+          </div>
         </div>
       </nav>
     )
