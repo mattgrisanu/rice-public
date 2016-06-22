132f984971b7973dd6f8e1526fa2f40e497625ca
Katherine <katherine.sj@gmail.com>
2016-06-21 17:28:09 -0700

    merged frontend-rice folder structure with new HoC

--- static/client/src/redux/rootReducer.js
+++ static/client/src/redux/rootReducer.js
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux';
-// import { restaurant, preferences } from './../modules/landing-view/ducks/ducks'
 import user from './UserReducer'
 import restaurant from './RestaurantReducer'
 import preferences from './PreferencesReducer'
+import _ from 'lodash';
+import { routerReducer } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   user: user,
   restaurant: restaurant,
   preferences: preferences,
+  form: formReducer,
+  routing: routerReducer
 
 });
 
