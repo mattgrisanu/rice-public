2589c02dd475e87bf77098c4e333b536f2adf62c
Daniel Fiore <taptapdan@gmail.com>
2016-06-22 19:48:52 -0700

    Get hard-coded Google Maps with pin displaying

--- static/client/src/modules/Restaurant-View/components/RestaurantView.entry.js
+++ static/client/src/modules/Restaurant-View/components/RestaurantView.entry.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import GoogleMap from 'google-map-react';
+import FontAwesome from 'react-fontawesome';
 
-class RestaurantViewEntry extends Component {
+const RestaurantViewEntry = React.createClass({
+  propTypes: {
+    initialZoom: React.PropTypes.number,
+    mapCenterLat: React.PropTypes.number,
+    mapCenterLng: React.PropTypes.number,
+    styles: React.PropTypes.object,
+  },
+
+  getDefaultProps() {
+    return {
+      initialZoom: 14,
+      mapCenterLat: 43.6425569,
+      mapCenterLng: -79.4073126,
+      styles: {
+        height: '400px',
+      },
+    };
+  },
+
   render() {
     return (
       <div>
         <h1>RestaurantViewEntry</h1>
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6" style={this.props.styles}>
+            <GoogleMap
+              defaultCenter={[this.props.mapCenterLat, this.props.mapCenterLng]}
+              defaultZoom={this.props.initialZoom}
+            >
+              <div className="place" lat={this.props.mapCenterLat} lng={this.props.mapCenterLng}>
+                <FontAwesome name="map-marker" size="2x" />
+                Place Name
+              </div>
+            </GoogleMap>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
       </div>
     );
-  }
-}
+  },
+});
 
 export default RestaurantViewEntry;
--- static/client/src/modules/Restaurant-View/containers/RestaurantView.js
+++ static/client/src/modules/Restaurant-View/containers/RestaurantView.js
@@ -19,7 +19,7 @@ class RestaurantView extends Component {
   render() {
     return (
       <div className="RestaurantView">
-        <RestaurantViewEntry />
+        <RestaurantViewEntry {...this.props} />
       </div>
     );
   }
