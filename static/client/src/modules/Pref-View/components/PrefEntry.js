72f3f66d31217e139fd7f54d0e12abdd271c0eb6
Daniel Fiore <taptapdan@gmail.com>
2016-07-01 19:46:31 -0700

    Add select cuisine style to PrefView

--- static/client/src/modules/Pref-View/components/PrefEntry.js
+++ static/client/src/modules/Pref-View/components/PrefEntry.js
@@ -3,14 +3,24 @@ import React, { Component } from 'react';
 export default class PrefEntry extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selected: false,
+    };
+  }
+
+  handleSelect() {
+    this.props.addPref(this.props.pref_id.name);
+    this.setState({ selected: true });
+    console.log('handleSelect', this.state);
   }
 
   render() {
     return (
       <div className="col-md-4">
         <div
-          onClick={function () {this.props.addPref(this.props.pref_id.name)}.bind(this)}
-          className={'option ' + this.props.pref_id.class}
+          onClick={this.handleSelect.bind(this)}
+          className={'option ' + this.props.pref_id.class + ' ' + (this.state.selected ? 'selected' : null)}
         >
           <span>{this.props.pref_id.name}</span>
         </div>
